1f5584e6637c03adecdc8c2bc5a66727ea7f0f78
oryschak <chris@bottled.io>
2015-02-20 21:59:13 -0800

    Ingredients API & ActiveAdmin functional

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
+  #Recipes API
+  resources :ingredients
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
