d1a585e37cda4a20e22c0bd4f00954a039cf7ce7
oryschak <chris@bottled.io>
2015-02-20 23:16:09 -0800

    messup

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -3,9 +3,4 @@ class AdminUser < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, 
          :recoverable, :rememberable, :trackable, :validatable
-
-  after_create { |admin| admin.send_reset_password_instructions}
-  def password_required?
-    new_record? ? false : super
-  end
 end
